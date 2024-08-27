abd038556758d5fb10b44c1dc814077e8efd6b7a
ltg-dyrs <ltg@dyrs.com>
2024-08-27 17:14:49 +0800

    增加node

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-# from .example_node import Example
+from .utools import UTools
 
 WEB_DIRECTORY = "./web"
 
-# NODE_CLASS_MAPPINGS = {
-#     "Example": Example
-# }
+NODE_CLASS_MAPPINGS = {
+    "UTools": UTools
+}
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
-# NODE_DISPLAY_NAME_MAPPINGS = {
-#     "Example": "Example Node"
-# }
+#  A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "UTools": "Load Image"
+}
 
-# __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
